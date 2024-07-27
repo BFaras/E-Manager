9d5a10e4d06c93a4411c48258b46d78e79cdcb69
moham <mohamed.ben-faras@polymtl.ca>
2024-07-26 22:05:10 -0400

    add webhook endpoint

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,66 @@
+import prismaDB from "@/lib/prismadb";
+import { headers } from "next/headers"
+import { NextResponse } from "next/server";
+import Stripe from "stripe"
+import {stripe} from "@/lib/stripe"
+
+
+export async function POST(req:Request) {
+    console.log("help")
+    const body = await req.text();
+    const signature = headers().get("Stripe-Signature") as string;
+
+    let event:Stripe.Event;
+    console.log("help")
+    try {
+        event = stripe.webhooks.constructEvent(body, signature, process.env.STRIPE_WEBHOOK_SECRET!);
+        console.log(event)
+    } catch (err:any) {
+        return new NextResponse(`WebhookError: ${err.message}` , {status: 400})
+    }
+
+    const session = event.data.object as Stripe.Checkout.Session;
+    const address = session?.customer_details?.address;
+
+    const addressComponents = [
+        address?.line1,
+        address?.line2,
+        address?.city,
+        address?.state,
+        address?.postal_code,
+        address?.country,
+    ]
+
+    const addressString = addressComponents.filter((c) => c !== null).join(', ')
+
+    if (event.type === "checkout.session.completed") {
+        const order = await prismaDB.order.update({
+            where:{
+                id: session?.metadata?.orderId
+            },
+            data: {
+                isPaid: true,
+                address: addressString,
+                phone: session?.customer_details?.phone || ' '
+            },
+            include: {
+                orderItems: true
+            }
+        })
+
+        const productsId = order.orderItems.map((orderItem) => orderItem.productId)
+
+        await prismaDB.product.updateMany({
+            where: {
+                id: { 
+                    in: [...productsId] }
+            },
+            data: {
+                isArchived:true
+            }
+        })
+    }   
+
+    return new NextResponse(null, {status: 200})
+
+}
